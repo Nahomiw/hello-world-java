a32239cde28eef6396fc8e241ccd3caf1bb5b4e3
Nahomiw <nahomib@gmail.com>
2022-09-08 01:01:09 -0500

    completed

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -8,7 +8,10 @@ public class HelloWorld {
     }
 
     public static String sayHello() {
-        return "";
+        return "Hello, World!";
     }
 
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
 }
